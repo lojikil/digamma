b403a970eab0d73ccc8f006cd5d82abcceae30e0
Stefan Edwards <saedwards.ecc@gmail.com>
2011-10-10 21:43:00 -0400

    pack test with string pointer

--- labs/tagruntime/pack0.c
+++ labs/tagruntime/pack0.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <gc.h>
+#include <string.h>
 
 #define TYPE(x) ((SExp)(x) & 0x1F)
 #define SET_TYPE(x,type) ((SExp)(x) += (type) & 0x1F)
 #define SET_INT(x,val) ((SExp)x) += (SExp)(val) << 32
-#define SET_FLOAT(x,val) ((SExp)x) += (SExp)(val) << 32
+#define SET_FLOAT(x,val) ((SExp)x += (SExp)*(&val) << 32)
+#define SET_STRING(x,val) x += ((SExp)(val) << 32)
 #define AINT(x) ((SExp)(x) >> 32)
 #define AFLOAT(x) (float)((SExp)(x) >> 32)
+#define GET_STRING(x) (String *)((SExp)(x) >> 32)
+
+#define hmalloc GC_MALLOC
+
+#define nil NULL
 
 typedef long long SExp;
 
@@ -16,11 +24,21 @@ typedef enum {
     T_CHAR, T_BOOL, T_GOAL
 } SExpType;
 
+typedef struct 
+{
+    int len;
+    char *data;
+} String;
+
 int
 main()
 {
+    GC_INIT();
     int val = 0;
-    float f = 0.0f;
+    double f = 0.0f;
+    char *str = hmalloc(sizeof(char) * 128);
+    String *g = hmalloc(sizeof(String));
+    String *h = nil;
     SExp sval = 0;
     printf("TYPE(x) == %d\n",TYPE(sval));
     SET_TYPE(sval,T_INTEGER);
@@ -35,5 +53,23 @@ main()
     SET_FLOAT(sval,f);
     printf("TYPE(x) == %d\n",TYPE(sval));
     printf("VALUE(x) == %f\n",AFLOAT(sval));
+    fgets(str,128,stdin);
+    printf("Please enter a string: ");
+    str = fgets(str,128,stdin);
+    g->data = str;
+    g->len = strlen(str);
+    printf("g->data == %s\n",g->data);
+    printf("g->len == %d\n",g->len);
+    printf("g == %x\n",g);
+    printf("sval: %llx\n",sval);
+    sval = 0l;
+    SET_STRING(sval,g);
+    SET_TYPE(sval,T_STRING);
+    printf("sval: %llx\n",sval);
+    h = GET_STRING(sval);
+    printf("h == %x\n",h);
+    printf("TYPE(x) == %d\n",TYPE(sval));
+    printf("VALUE(x) == %s\n",h->data);
+    printf("LEN(x) == %d\n",h->len);
     return 0;
 }
